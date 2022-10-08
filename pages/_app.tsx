7da488cd541373da6d7cbac9ea79f5bfc6b57a0c
Michelle Poon <hjklqw@yahoo.ca>
2022-10-07 18:20:00 -0600

    Pages now smooth scroll to the top on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import { Router } from 'next/router'
 import { RecoilRoot } from 'recoil'
 
 import 'common/styles/global.scss'
@@ -7,28 +8,39 @@ import { GlobalLayout } from '/common/layouts/global'
 import { Head } from '/common/head'
 import { PageModel } from '/common/models/page'
 import { PageLayout } from '/common/layouts/page'
+import { getScrollRoot } from '/utils/document'
 
 type Props<P = any> = AppProps<P> & {
   Component: PageModel<P>
 }
 
-const App = ({ Component, pageProps }: Props) => (
-  <RecoilRoot>
-    <Head
-      title={Component.title}
-      description={Component.description}
-      structuredData={Component.meta}
-    />
-    <GlobalLayout>
-      {Component.usePageLayout !== false ? (
-        <PageLayout title={Component.title} subtitle={Component.subtitle}>
+const App = ({ Component, pageProps }: Props) => {
+  Router.events.on('routeChangeComplete', () => {
+    getScrollRoot()?.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    })
+  })
+
+  return (
+    <RecoilRoot>
+      <Head
+        title={Component.title}
+        description={Component.description}
+        structuredData={Component.meta}
+      />
+      <GlobalLayout>
+        {Component.usePageLayout !== false ? (
+          <PageLayout title={Component.title} subtitle={Component.subtitle}>
+            <Component {...pageProps} />
+          </PageLayout>
+        ) : (
           <Component {...pageProps} />
-        </PageLayout>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </GlobalLayout>
-  </RecoilRoot>
-)
+        )}
+      </GlobalLayout>
+    </RecoilRoot>
+  )
+}
 
 export default App
